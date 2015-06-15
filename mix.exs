0134390ecf988eebd28977fe45a744570f83367c
RAWdaMedia <info@rawdamedia.com>
2015-06-16 01:37:14 +1000

    updated version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Quoil.Mixfile do
 
   def project do
     [app: :quoil,
-     version: "0.3.0",
+     version: "0.3.2",
      elixir: "~> 1.0",
      escript: escript_config,
      build_embedded: Mix.env == :prod,

025d225e960354507be7ea52d6fdcd731ff9b790
RAWdaMedia <info@rawdamedia.com>
2015-07-29 23:38:46 +1000

    testing gen_icmp
    can’t seem to get gen_icmp working - don’t know if it is a mac specific
    problem -> will try linux

--- test/ping_test.exs
+++ test/ping_test.exs
@@ -4,28 +4,23 @@ defmodule PingTest do
   import String
   import ExUnit.CaptureIO
 
-  @tag timeout: 10000000
+  # Testing run_ping function using system ping utility
 
-  # Testing run_ping function
-
-  test "able to run ping function" do
+  test "able to run system ping function" do
     test_data = capture_io(fn -> Quoil.CLI.main(["-i","1","-n","3","-s","google.com"]) end)
     IO.puts test_data
     assert (contains?(test_data, "\nPING statistics")) && (contains?(test_data, "google.com"))
   end
 
-  # test "asking for help terminates the program" do
-  #   IO.puts ""
-  #   assert catch_exit(main(["--help"])) == 0
-  # end
+  # Testing gen_icmp
 
-  # test "passing unknown switches terminates the program" do
-  #   IO.puts ""
-  #   assert catch_exit(main(["--unknown"])) == 0
-  # end
+  test "able to use gen_icmp" do 
+    IO.puts "--==<< Testing gen_icmp >>==--"
+    {:ok, socket} = :gen_icmp.open()
+    IO.puts inspect(socket)
+    response = :gen_icmp.ping(socket,['www.google.com.au', 'telstra.com.au'],[])
+    IO.puts inspect(response)
+    assert true
+  end
 
-  # test "terminate_early? exits the program on receiving :help" do
-  #   IO.puts ""
-  #   assert catch_exit(terminate_early?(:help)) == 0
-  # end
 end
